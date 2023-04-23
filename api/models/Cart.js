eb14128edec81ca2ec25ab4acde8b59158694ce8
migdalius <nyjepl@gmail.com>
2023-04-23 16:49:11 +0200

    cart changes

--- api/models/Cart.js
+++ api/models/Cart.js
@@ -8,6 +8,12 @@ const CartSchema = new mongoose.Schema(
         productId: {
           type: String,
         },
+        productName: {
+          type: String,
+        },
+        productPrice: {
+          type: Number,
+        },
         quantity: {
           type: Number,
           default: 1,
