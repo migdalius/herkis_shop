60cc6ad20aa43f0d005a1d382a314db9dea4eeff
migdalius <nyjepl@gmail.com>
2023-04-23 16:18:05 +0200

    baselinker send order

--- client/src/components/orderTable/OrderTable.jsx
+++ client/src/components/orderTable/OrderTable.jsx
@@ -16,9 +16,64 @@ import { userRequest } from "../../pages/requestMethods";
 import { Link } from "react-router-dom";
 
 const OrderTable = ({ orders }) => {
-  const handleGetOrder = async () => {
+  const handleGetOrder = async (order) => {
+    const products = order.products.map((product) => {
+      return {
+        storage: "db",
+        storage_id: 0,
+        product_id: `${product.productId}`,
+        variant_id: "",
+        location: "",
+        name: "test",
+        sku: "",
+        ean: `${product.productId}`,
+        price_brutto: 20.0,
+        tax_rate: 23,
+        quantity: `${product.quantity}`,
+        weight: 1,
+      };
+    });
     try {
-      const response = await userRequest.post("baselinker/neworder");
+      const response = await userRequest.post("baselinker/neworder", {
+        methodParams: {
+          order_status_id: "204874",
+          date_add: "1682182954",
+          user_comments: "",
+          admin_comments: "Zamówienie wygenerowane przez herkis.com",
+          phone: "000000000",
+          email: `${order.address.email}`,
+          user_login: `${order.address.name}`,
+          currency: "PLN",
+          payment_method: "Dotpay",
+          payment_method_cod: "0",
+          paid: "1",
+          delivery_method: "Kurier Inpost",
+          delivery_price: "14.99",
+          delivery_fullname: `${order.address.name}`,
+          delivery_company: "",
+          delivery_address: `${order.address.delivery}`,
+          delivery_city: `${order.address.city}`,
+          delivery_state: "",
+          delivery_postcode: `${order.address.zip}`,
+          delivery_country_code: "PL",
+          delivery_point_id: "",
+          delivery_point_name: "",
+          delivery_point_address: "",
+          delivery_point_postcode: "",
+          delivery_point_city: "",
+          invoice_fullname: `${order.address.name}`,
+          invoice_company: "",
+          invoice_nip: "",
+          invoice_address: `${order.address.delivery}`,
+          invoice_city: `${order.address.city}`,
+          invoice_state: "",
+          invoice_postcode: `${order.address.zip}`,
+          invoice_country_code: "PL",
+          want_invoice: "0",
+          products: products,
+        },
+      });
+      console.log(order);
       console.log(response.data);
     } catch (error) {
       console.error(error);
@@ -96,7 +151,10 @@ const OrderTable = ({ orders }) => {
                     </div>
                   </td>
                   <th>
-                    <button onClick={handleGetOrder}>Baselinker</button>
+                    {/* <button onClick={handleGetOrder}>Baselinker</button> */}
+                    <button onClick={() => handleGetOrder(order)}>
+                      Baselinker
+                    </button>
                   </th>
 
                   <th>
